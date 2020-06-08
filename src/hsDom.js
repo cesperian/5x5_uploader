9dbe58af937476c0069e2defb31667a4e1ad7b07
Chris <cesperian@gmail.com>
2020-06-08 18:25:06 +0700

    finish functional blocks, css, view

--- src/hsDom.js
+++ src/hsDom.js
@@ -1,13 +1,44 @@
 import hh from 'hyperscript-helpers';
 import h from 'hyperscript';
 
-const {div, span, img, label, input, select, option, br, button} = hh(h);
+const {div, span, img, label, input, select, option, br, button, p, b} = hh(h);
 
 export default function ddDom(dispatch, model){
+
+    if(model.numberAlert || model.sizeAlert.show) {
+        setTimeout(() => {
+            const el = document.querySelector('#modal1');
+            const modal = M.Modal.init(el);
+            modal.open();
+        }, 50);
+    }
+
     return div('#uploaderCont', [
         ddCont(dispatch),
         queueCont(dispatch, model),
-        submitCont(dispatch, model)
+        submitCont(dispatch, model),
+        div('#modal1.modal', [
+            div('.modal-content', [
+                p(model.numberAlert ? `
+                    The limit for the number of file uploads is ${model.fileLimit}
+                ` : `
+                    The size limit for individual files is ${model.sizeLimit} MB.
+                `),
+                () => {
+                    if (!model.numberAlert) {
+                        return p([
+                            b(model.sizeAlert.name),
+                            ` is ${(model.sizeAlert.size/1048576).toFixed(1)} MB`
+                            ]);
+                    } else {
+                        return br();
+                    }
+                }
+            ]),
+            div('.modal-footer', [
+                button('.modal-close.waves-effect.waves-green.btn-flat', 'Close')
+            ])
+        ])
     ]);
 }
 
@@ -15,8 +46,6 @@ function ddCont(dispatch){
     return div('#ddArea.container', [
         div('.row.ddHandler', [
             div('#dragandrophandler.col.s12.valign-wrapper', {
-                // ondragenter: (e) => e.target.classList.add('active'),
-                // ondragover: (e) => e.target.classList.add('active'),
                 ondragenter: (e) => {
                     e.stopPropagation();
                     e.preventDefault();
@@ -52,47 +81,66 @@ function ddCont(dispatch){
     ]);
 }
 
-function queueCont(dispatch, model) {
-    // console.log('queuecont model', model);
-    return div('#queue.container', model.map( file =>
-        div('.row', [
-            div('.col.s3.name.valign-wrapper', [
-                span('.truncate', file.name)
-            ]),
-            div('.col.s3.desc.valign-wrapper', [
-                input({
-                    type: 'text',
-                    placeholder: 'Description of file',
-                    maxLength: '100'
-                }),
-            ]),
-            div('.col.s3.options.valign-wrapper', [
-                select({style: 'display:inline-block'},[
-                    option({
-                        value: '',
-                        disabled: 'disabled',
-                        selected: 'selected'
-                    }, 'Select this file type'),
-                    option({
-                        value: '',
-                    }, 'Select this file type'),
-                    option({
-                        value: '',
-                    }, 'Select this file type')
-                ])
-            ]),
-            div('.col.s3.remove.valign-wrapper', [
-                button(
-                    '.btn.btn-small.waves-effect.waves-light.red',
-                    {
-                        name: file.name,
-                        onclick: (e) => dispatch('remove', e)
-                    },
-                    'remove')
-            ])
+function buildQueueCols(model, file, dispatch) {
+    let row = [
+        div('.col.s3.name.valign-wrapper', [
+            span('.truncate', file.name)
         ])
-    ));
+    ];
+    if (model.showDescription) row.push(
+        div('.col.s3.desc.valign-wrapper', [
+            input({
+                type: 'text',
+                placeholder: 'Description of file',
+                maxLength: '100',
+                name: file.name,
+                value: file.description,
+                onchange: (e) => dispatch('updateDesc', e)
+            }),
+        ])
+    );
+    if (model.selectOpts) row.push(
+        div('.col.s3.options.valign-wrapper', [
+            select({
+                style: 'display:inline-block',
+                name: file.name,
+                value: file.fileType,
+                onchange: (e) => dispatch('updateType', e)
+            }, buildFileTypeOpts(model, file))
+        ])
+    );
+    row.push(
+        div('.col.s3.remove.valign-wrapper', [
+            button(
+                '.btn.btn-small.waves-effect.waves-light.red',
+                {
+                    name: file.name,
+                    onclick: (e) => dispatch('remove', e)
+                },
+                'remove')
+        ])
+    );
+    return row;
 }
+
+function buildFileTypeOpts(model, file) {
+    let defaultOpt = {value: '', disabled: 'disabled'};
+    if(!file.fileType) defaultOpt = {selected: 'selected', ...defaultOpt};
+    let options = [option(defaultOpt, 'Select this file type')];
+    Object.keys(model.selectOpts).forEach( key => {
+        let optionTemplate = {value: key};
+        if(file.fileType == key) optionTemplate = {selected: 'selected', ...optionTemplate};
+        options.push(option(optionTemplate, model.selectOpts[key]));
+    });
+    return options;
+}
+
+function queueCont(dispatch, model) {
+    console.log('queuecont model', model);
+    return div('#queue.container', model.files.map( file =>
+            div('.row', buildQueueCols(model, file, dispatch))
+        ));
+} // queueCont
 
 function submitCont(dispatch, model) {
     const submitDom = div('#submit.container', [
@@ -105,5 +153,5 @@ function submitCont(dispatch, model) {
             ])
         ])
     ]);
-    return model.length ? submitDom : '';
+    return model.files.length ? submitDom : '';
 }
--- src/index.js
+++ src/index.js
@@ -1,63 +1,96 @@
 
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize.js';
+
 import view from './hsDom';
 import './ul.scss';
 
 (function(){
 
 
-    const initFileList = [];
+    const initModel = {
+        destination:null,
+        destinationParams:null,
+        sizeLimit:1,
+        fileLimit:5,
+        selectOpts:null,
+        showDescription:false,
+        // postFn:$.noop,
+        files: [],
+        sizeAlert: {
+            show: false,
+            name: null,
+            size: null
+        },
+        numberAlert: false
+    };
 
     function updateModel(msg, model, e) {
+        model.numberAlert = model.sizeAlert.show = false;
         switch (msg) {
             case 'manualSelect': return queueFiles(e.srcElement.files, model);
-            case 'remove': return model.filter(f => f.name !== e.srcElement.attributes['name'].value);
+            case 'remove': return removeFile(model, e);
             case 'submit': return submitFiles(model);
             case 'dropfile': return queueFiles(e.dataTransfer.files, model);
-            // case 'dropfile': return model;
+            case 'updateDesc': return updateDesc(model, e);
+            case 'updateType': return updateType(model, e);
             default: return model;
         }
     }
 
+    function updateType(model, e){
+        console.log('selCh', e);
+        const name = e.target.attributes['name'].value;
+        const val = e.target.value;
+        model.files.map(f => {
+            if (f.name === name) f.fileType = val;
+            return f;
+        });
+        return model;
+    }
+
+    function updateDesc(model, e) {
+        const name = e.target.attributes['name'].value;
+        const val = e.target.value;
+        model.files.map(f => {
+            if (f.name === name) f.description = val;
+            return f;
+        });
+        return model;
+    }
+
+    function removeFile(model, e) {
+        model.files = model.files.filter(f => f.name !== e.srcElement.attributes['name'].value);
+        return model;
+    }
+
     function queueFiles(fl, model) {
-        // if (manifest.length == opts.fileLimit || opts.fileLimit < manifest.length + fl.length) {
-        //     const limitMsg = `The limit for number of file uploads is ${opts.fileLimit}.`;
-        //     const $modal = $('.modal');
-        //     $modal.find('.modal-body').html(limitMsg);
-        //     $modal.modal('show');
-        //     return;
-        // }
-        // let $src = $(".queueSrc").html();
+        if (model.files.length == model.fileLimit || model.fileLimit < model.files.length + fl.length) {
+            model.numberAlert = true;
+            return model;
+        }
         for (let i =0; i < fl.length; i++) {
-            // if (fl[i].size > (opts.sizeLimit * 1048576) || false ) {
-            if (false ) {
-                // const sizeMsg = `
-                //     The size limit for individual files is ${opts.sizeLimit} MB.
-                //     <br><b>${fl[i].name}</b> is ${(fl[i].size/1048576).toFixed(1)} MB.`
-                // const $modal = $('.modal');
-                // $modal.find('.modal-body').html(sizeMsg);
-                // $modal.modal('show');
-                return;
+            console.log('sizeFl', fl[i]);
+            if (fl[i].size > (model.sizeLimit * 1048576)) {
+                model.sizeAlert.show = true;
+                model.sizeAlert.name = fl[i].name;
+                model.sizeAlert.size = fl[i].size;
+                return model;
             }else{
-                // $("<div class='row fileQueue'></div>")
-                //     .html($src)
-                //     .find('.col.name b').text(fl[i].name).end()
-                //     .find('.remove input').attr('data-name', fl[i].name).end()
-                //     .insertAfter(".row.ddHandler")
-                //     .animate({opacity:1},850);
-                model.push(fl[i]);
+                model.files.push(fl[i]);
             }
         }
-        // $('.row.submit').css('display','flex');
         // console.log('model', model);
         return model;
     }
 
     // impure function block...
     // todo; why does updatemodel need to be passed in as an arg? Cant be just called from dispatch?
-    function app(initFileList, updateModel, view, node) {
-        let model = [];
+    function app(initModel, updateModel, view, node) {
+        const config = JSON.parse(node.querySelector('#config').innerHTML);
+        let model = {...initModel, ...config};
+        if (model.showDescription) File.prototype.description = '';
+        if (model.selectOpts) File.prototype.fileType = '';
 
         // send dispatch as arg so can be specified in events created in view fn..
         // for initial view..
@@ -67,8 +100,8 @@ import './ul.scss';
         // ...and updated view (dispatch nested here to isolate side effects) ...
         function dispatch(msg, e) {
             model = updateModel(msg, model, e);
-            // console.log('dispatch model', model);
-            console.log('dispatch e', e);
+            console.log('dispatch model', model);
+            // console.log('dispatch e', e);
             const updatedView = view(dispatch, model);
             node.replaceChild(updatedView, currentView);
             currentView = updatedView;
@@ -77,7 +110,7 @@ import './ul.scss';
 
     window.addEventListener('DOMContentLoaded', () => {
         const rootNode = document.querySelector('#uploader');
-        app(initFileList, updateModel, view, rootNode);
+        app(initModel, updateModel, view, rootNode);
     });
 
 }());
