2aa51c6d52b824b20734f929e3886d5b07e16a77
Chris <cesperian@gmail.com>
2021-02-11 16:42:57 +0700

    Fix materialize import, update config for webpack ^5

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,13 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "5x5_uploader.min.js"
     },
+    resolve: {
+        fallback: {
+            "querystring": require.resolve("querystring-es3")
+        }
+    },
     node: {
-        fs: "empty" //https://github.com/pugjs/pug-loader/issues/8
+        // fs: "empty" //https://github.com/pugjs/pug-loader/issues/8
     },
     module: {
         rules: [
